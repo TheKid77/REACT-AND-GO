da9ba2999d76eeaa529dfbf6e63e2e20bd172609
TheKid77 <andymcc77@blueyonder.co.uk>
2024-09-01 09:21:27 +0100

    Basic API

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprint(w, "Web Services are easy with Go!")
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
