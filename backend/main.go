4a5073b29bd904d0fd125dab976423784be4915d
TheKid77 <andymcc77@blueyonder.co.uk>
2024-09-02 16:52:02 +0100

    Add Flags and parse a file

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
+	level := flag.String("level", "CRITICAL", "log level to filter for")
+	flag.Parse()
+	fmt.Println("Level contains", *level)
+	
+	f, err := os.Open("./log.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	bufReader := bufio.NewReader(f)
+
+	for line, err := bufReader.ReadString('\n'); err == nil; line, err = bufReader.ReadString('\n') {
+		if strings.Contains(line, *level) {
+			fmt.Print(line)
+		}
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, "Web Services are easy with Go!")
 	})
@@ -15,4 +38,5 @@ func main() {
 	})
 
 	http.ListenAndServe(":8000", nil)
+
 }
